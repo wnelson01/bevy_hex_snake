3b3912bc346bf17d39ad206db221b7ca6c56e582
wnelson <wnelson@wnelson.jax.office.monetra.com>
2021-12-14 17:15:46 -0500

    input

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,36 @@ struct Hex {
     r: f32,
 }
 
+struct Head;
+
+struct Tail;
+
+struct Food;
+
+struct InputHandler {
+    action: Action,
+}
+
+#[derive(Debug)]
+enum Action {
+    Up,
+    UpRight,
+    Right,
+    DownRight,
+    Down,
+    DownLeft,
+    Left,
+    UpLeft,
+    None,
+}
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(setup.system())
+        .add_system(render_hexes.system())
+        .add_system(action_system.system())
         .run();
 }
 
@@ -34,21 +58,97 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    let map = generate_map(3);
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.transform.scale = Vec3::new(2.0, 2.0, 1.0);
+    commands.spawn_bundle(camera);
+
+    let map = generate_map(4);
     let texture_handle = asset_server.load_folder("HK-Heightend Sensory Input v2/HSI - Indigo/").unwrap();
     for hex in map {
         commands.spawn_bundle(SpriteBundle {
             material: materials.add((texture_handle[thread_rng().gen_range(0..texture_handle.len())]).clone().typed().into()),
-            transform: Transform {
-                translation: Vec3::new(
-                    52.5 * (3.0_f32.sqrt() * hex.q + (3 as f32).sqrt() / 2. * hex.r),
-                    -52.5 * (3./2. * hex.r),
-                    0.0,
-                ),
-                scale: Vec3::new(0.5, 0.5, 1.0),
-                ..Default::default()},
             ..Default::default()
         }).insert(hex);
+    }
+
+    let texture_handle = asset_server.load("HK-Heightend Sensory Input v2/HSI - Icons/HSI - Icon Geometric Light/HSI_icon_123l.png");
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(texture_handle.into()),
+        ..Default::default()
+    })
+    .insert(Hex { q: 0., r: 0. })
+    .insert(Head)
+    .insert(InputHandler { action: Action::None });
+}
+
+fn render_hexes(
+    mut query: Query<(&Hex, &mut Transform), Changed<Transform>>,
+) {
+    for (hex, mut transform) in query.iter_mut() {
+        transform.translation = Vec3::new(
+            105. * (3.0_f32.sqrt() * hex.q + (3 as f32).sqrt() / 2. * hex.r),
+            -105. * (3./2. * hex.r),
+            0.0,
+        )
+    }
+}
+
+fn action_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut InputHandler>
+) {
+    if let Some(mut input_handler) = query.iter_mut().next() {
+        if 
+            keyboard_input.pressed(KeyCode::Up) && 
+            !keyboard_input.pressed(KeyCode::Right) && 
+            !keyboard_input.pressed(KeyCode::Down) && 
+            !keyboard_input.pressed(KeyCode::Left) {
+                input_handler.action = Action::Up;
+        } else if 
+            keyboard_input.pressed(KeyCode::Up) && 
+            keyboard_input.pressed(KeyCode::Right) && 
+            !keyboard_input.pressed(KeyCode::Down) &&
+            !keyboard_input.pressed(KeyCode::Left) {
+                input_handler.action = Action::UpRight;
+        } else if 
+            !keyboard_input.pressed(KeyCode::Up) &&
+            keyboard_input.pressed(KeyCode::Right) && 
+            !keyboard_input.pressed(KeyCode::Down) && 
+            !keyboard_input.pressed(KeyCode::Left) {
+                input_handler.action = Action::Right;
+        } else if 
+            !keyboard_input.pressed(KeyCode::Up) &&
+            keyboard_input.pressed(KeyCode::Right) && 
+            keyboard_input.pressed(KeyCode::Down) && 
+            !keyboard_input.pressed(KeyCode::Left) {
+                input_handler.action = Action::DownRight;
+        } else if
+            !keyboard_input.pressed(KeyCode::Up) &&
+            !keyboard_input.pressed(KeyCode::Right) && 
+            keyboard_input.pressed(KeyCode::Down) && 
+            !keyboard_input.pressed(KeyCode::Left) {
+                input_handler.action = Action::Down;
+        } else if
+            !keyboard_input.pressed(KeyCode::Up) &&
+            !keyboard_input.pressed(KeyCode::Right) && 
+            keyboard_input.pressed(KeyCode::Down) && 
+            keyboard_input.pressed(KeyCode::Left) {
+                input_handler.action = Action::DownLeft;
+        } else if
+            !keyboard_input.pressed(KeyCode::Up) &&
+            !keyboard_input.pressed(KeyCode::Right) && 
+            !keyboard_input.pressed(KeyCode::Down) && 
+            keyboard_input.pressed(KeyCode::Left) {
+                input_handler.action = Action::Left;
+        } else if
+            keyboard_input.pressed(KeyCode::Up) &&
+            !keyboard_input.pressed(KeyCode::Right) && 
+            !keyboard_input.pressed(KeyCode::Down) && 
+            keyboard_input.pressed(KeyCode::Left) {
+                input_handler.action = Action::UpLeft;
+        } else {
+            input_handler.action = Action::None;
+        }
+        println!("{:?}", input_handler.action);
     }
 }
