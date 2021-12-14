40e5e0535fdb4f0e03ee9eeee216a122c6ef56ea
wnelson <wnelson@wnelson.jax.office.monetra.com>
2021-12-14 10:51:31 -0500

    random hex textures

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use bevy_inspector_egui::WorldInspectorPlugin;
+use rand::{thread_rng, Rng};
 
 #[derive(Debug)]
 struct Hex {
@@ -6,9 +8,11 @@ struct Hex {
     r: f32,
 }
 
+
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(setup.system())
         .run();
 }
@@ -30,18 +34,12 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let map = generate_map(3);
-    // let mut map: Vec<Hex> =  vec![Hex { q: 0.0, r: 0.0 }];
-    // map.push(Hex {q: 1.0, r: 0.0});
-    // map.push(Hex {q: 0.0, r: 1.0});
-    // map.push(Hex {q: 1.0, r: 1.0 });
-    // map.push(Hex {q: -3.0, r: 3.0});
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    let map = generate_map(3);
     let texture_handle = asset_server.load_folder("HK-Heightend Sensory Input v2/HSI - Indigo/").unwrap();
-    println!("{:?}", map);
     for hex in map {
         commands.spawn_bundle(SpriteBundle {
-            material: materials.add((texture_handle[0]).clone().typed().into()),
+            material: materials.add((texture_handle[thread_rng().gen_range(0..texture_handle.len())]).clone().typed().into()),
             transform: Transform {
                 translation: Vec3::new(
                     52.5 * (3.0_f32.sqrt() * hex.q + (3 as f32).sqrt() / 2. * hex.r),
@@ -50,14 +48,7 @@ fn setup(
                 ),
                 scale: Vec3::new(0.5, 0.5, 1.0),
                 ..Default::default()},
-            // transform: Transform::from_translation(
-            //     Vec3::new(
-            //         105. * ((3 as f32).sqrt() * hex.q as f32 + (3 as f32).sqrt() / 2. * hex.r as f32),
-            //         105. * (3./2. * hex.r as f32),
-            //         0.0,
-            //     ),
-            // ),
             ..Default::default()
-        });
+        }).insert(hex);
     }
 }
