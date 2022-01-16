3e0bafd77ba04c64e54c5b36bebf171348f04a90
nelsonwa <nelsonwa@oregonstate.edu>
2022-01-16 16:13:51 -0500

    head movement

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
-use bevy_inspector_egui::WorldInspectorPlugin;
+use bevy_inspector_egui::{Inspectable, WorldInspectorPlugin, RegisterInspectable};
 use rand::{thread_rng, Rng};
 
-#[derive(Component, Debug)]
+#[derive(Component, Inspectable)]
 struct Hex {
     q: f32,
     r: f32,
+    z: f32
 }
 
 #[derive(Component)]
@@ -39,9 +40,11 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
-        .add_startup_system(setup.system())
-        .add_system(render_hexes.system())
-        .add_system(action_system.system())
+        .register_inspectable::<Hex>()
+        .add_startup_system(setup)
+        .add_system(hex_to_pixel)
+        .add_system(action_system)
+        .add_system(head_movement)
         .run();
 }
 
@@ -50,7 +53,7 @@ fn generate_map(x: isize) -> Vec<Hex> {
     for i in (-1 * x)..x+1 {
         for j in -x..x+1 {
             if (i + j).abs() <= x {
-                map.push(Hex{ q: i as f32, r: j as f32 });
+                map.push(Hex{ q: i as f32, r: j as f32, z: 0. });
             }
         }
     }
@@ -79,19 +82,20 @@ fn setup(
         texture: texture_handle,
         ..Default::default()
     })
-    .insert(Hex { q: 0., r: 0. })
+    .insert(Hex { q: 0., r: 0., z: 1. })
     .insert(Head)
     .insert(InputHandler { action: Action::None });
 }
 
-fn render_hexes(
-    mut query: Query<(&Hex, &mut Transform), Changed<Transform>>,
+/// Convert hex to pixel
+fn hex_to_pixel(
+    mut query: Query<(&Hex, &mut Transform), Changed<Hex>>,
 ) {
     for (hex, mut transform) in query.iter_mut() {
         transform.translation = Vec3::new(
             105. * (3.0_f32.sqrt() * hex.q + (3 as f32).sqrt() / 2. * hex.r),
             -105. * (3./2. * hex.r),
-            0.0,
+            hex.z,
         )
     }
 }
@@ -151,6 +155,38 @@ fn action_system(
                 input_handler.action = Action::UpLeft;
         } else {
             input_handler.action = Action::None;
+        }
+    }
+}
+
+fn head_movement(
+    mut query: Query<(&mut Hex, &InputHandler), With<Head>>
+) {
+    for (mut hex, input_handler) in query.iter_mut() {
+        match input_handler.action {
+            Action::Up => (),
+            Action::UpRight => {
+                hex.q += 1.;
+                hex.r -= 1.;
+            },
+            Action::Right => {
+                hex.q += 1.;
+            },
+            Action::DownRight => {
+                hex.r += 1.;
+            },
+            Action::DownLeft => {
+                hex.q -= 1.;
+                hex.r += 1.;
+            },
+            Action::Down => (),
+            Action::Left => {
+                hex.q -= 1.;
+            },
+            Action::UpLeft => {
+                hex.r -= 1.;
+            },
+            Action::None => (),
         }
     }
 }
