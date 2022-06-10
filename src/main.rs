5dfb16ab3008ebb4de721b043cdd969e09e38694
wnelson <wnelson@wnelson2.wnelson2.p10jax.office.monetra.com>
2022-06-10 14:47:49 -0400

    peer connection establish

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, tasks::IoTaskPool};
 // use bevy_inspector_egui::{Inspectable, WorldInspectorPlugin, RegisterInspectable};
 use rand::{thread_rng, Rng};
 use bevy::input::keyboard::KeyboardInput;
 use bevy::core::FixedTimestep;
 // use bevy_editor_pls::prelude::*;
+use matchbox_socket::WebRtcSocket;
 
 struct CrumpleHandle(Handle<Image>);
 
@@ -72,6 +73,8 @@ fn main() {
         // .register_inspectable::<Hex>()
         .add_startup_system(generate_map)
         .add_startup_system(spawn_initial_crumple)
+        .add_startup_system(start_matchbox_socket)
+        .add_system(wait_for_players)
         .add_system(spawn_crumple)
         .add_startup_system(setup)
         .add_startup_system(spawn_snake)
@@ -151,6 +154,38 @@ fn spawn_snake(
     .insert(HexHistory(Vec::new()))
     .insert(Head { direction: Direction::None, last_direction: Direction::None })
     .insert(Tail);
+}
+
+fn start_matchbox_socket(mut commands: Commands, task_pool: Res<IoTaskPool>) {
+    let room_url = "ws://127.0.0.1:3536/next_2";
+    info!("connecting to matchbox server: {:?}", room_url);
+    let (socket, message_loop) = WebRtcSocket::new(room_url);
+
+    // The message loop needs to be awaited, or nothing will happen.
+    // We do this here using bevy's task system.
+    task_pool.spawn(message_loop).detach();
+
+    commands.insert_resource(Some(socket));
+}
+
+fn wait_for_players(mut socket: ResMut<Option<WebRtcSocket>>) {
+    let socket = socket.as_mut();
+
+    // If there is no socket we've already started the game
+    if socket.is_none() {
+        return;
+    }
+
+    // Check for new connections
+    socket.as_mut().unwrap().accept_new_connections();
+    let players = socket.as_ref().unwrap().players();
+
+    let num_players = 2;
+    if players.len() < num_players {
+        return; // wait for more players
+    }
+
+    info!("All peers have joined, going in-game");
 }
 
 /// Convert hex to pixel
